e47d4bf090bc62a511a503e6890bb1d3047c2ad3
Mike Woods <mike.woods@ncontracts.com>
2021-12-18 22:50:32 -0600

    Day 9 part 2

---
+++ Day09/Basin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Day09
+{
+    public class Basin
+    {
+        public List<Location> Locations { get; set; } = new();
+
+        public bool HasLocation(int x, int y)
+        {
+            return Locations.Any(loc => loc.X == x && loc.Y == y);
+        }
+
+        public void AddLocation(int x, int y)
+        {
+            Locations.Add(new Location{X = x, Y = y});
+        }
+    }
+}
--- Day09/Solver.cs
+++ Day09/Solver.cs
@@ -35,9 +35,53 @@ namespace Day09
                 .Sum();
         }
 
-        // public int Solve2()
-        // {
-        // }
+        public int Solve2()
+        {
+            var basins = new List<Basin>();
+
+            for (var i = 0; i < _points.GetLength(0); i++)
+            {
+                for (var j = 0; j < _points.GetLength(1); j++)
+                {
+                    if (_points[i, j] == 9) continue;
+
+                    if (basins.Any(b => b.HasLocation(i, j))) continue;
+
+                    var basin = new Basin();
+                    BuildBasin(i, j, basin);
+
+                    basins.Add(basin);
+                }
+            }
+
+            var threeLargest = basins
+                .OrderByDescending(b => b.Locations.Count)
+                .Take(3)
+                .ToList();
+
+            return threeLargest
+                .Select(b => b.Locations.Count)
+                .Aggregate((product, next) => product * next);
+        }
+
+        void BuildBasin(int x, int y, Basin basin)
+        {
+            if (x < 0 || y < 0 
+                      || x > _points.GetLength(0) - 1 
+                      || y > _points.GetLength(1) - 1 
+                      || _points[x,y] == 9
+                      || basin.HasLocation(x, y))
+            {
+                return;
+            }
+
+            basin.AddLocation(x, y);
+
+            BuildBasin(x, y - 1, basin);
+            BuildBasin(x, y + 1, basin);
+            BuildBasin(x - 1, y, basin);
+            BuildBasin(x + 1, y, basin);
+        }
 
         bool IsLocalMin(int x, int y)
         {
